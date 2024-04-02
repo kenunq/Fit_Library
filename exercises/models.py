25f14a442078b2446b5268d0bb851f9c684b3df2
kenunq <ivaiika@yandex.ru>
2024-04-02 14:32:08 +0300

    update choices

--- exercises/models.py
+++ exercises/models.py
@@ -1,22 +1,24 @@
 from django.db import models
 
 
+class ExerciseTypeChoices(models.TextChoices):
+    CARDIO = "Кардио"
+    POWER = "Силовые"
+    STRETCHING = "Растяжка"
+    AEROBICS = "Аэробика"
+
+
+class ExerciseDifficultyChoices(models.TextChoices):
+    EASY = "Для начинающих"
+    MEDIUM = "Средний"
+    HARD = "Продвинутый"
+
+
 class Exercise(models.Model):
-    EXERCISE_TYPE_CHOICES = (
-        ("Кардио", "Кардио"),
-        ("Силовые", "Силовые"),
-        ("Растяжка", "Растяжка"),
-        ("Аэробика", "Аэробика"),
-    )
-    EXERCISE_DIFFICULTY_CHOICES = (
-        ("Для начинающих", "Для начинающих"),
-        ("Средний", "Средний"),
-        ("Продвинутый", "Продвинутый"),
-    )
     name = models.CharField(max_length=64, verbose_name="Название упражнения")
     description = models.CharField(max_length=500, verbose_name="Описание")
-    type_exercise = models.CharField(max_length=50, choices=EXERCISE_TYPE_CHOICES, verbose_name="Тип упражнения")
-    difficulty = models.CharField(max_length=50, choices=EXERCISE_DIFFICULTY_CHOICES, verbose_name="Уровень сложности")
+    type_exercise = models.CharField(max_length=50, choices=ExerciseTypeChoices, verbose_name="Тип упражнения")
+    difficulty = models.CharField(max_length=50, choices=ExerciseDifficultyChoices, verbose_name="Уровень сложности")
     duration = models.CharField(max_length=50, verbose_name="Продолжительность выполнения")
     repeats = models.CharField(max_length=100, verbose_name="Количество повторений и подходов")
 
